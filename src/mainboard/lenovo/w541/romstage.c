5766eaff6ad2a91063e277894d1f90267186d213
Angel Pons <th3fanbus@gmail.com>
2020-07-03 23:32:44 +0200

    mb/lenovo/w541/romstage: Automatically determine system type
    Check the PCH's LPC device ID to know the system type instead of relying
    on hardcoded numbers. The `get_pch_platform_type` function is MRC-safe.
    
    Change-Id: I358a941acb0c5d2e72edb397a7effc837dcf11fd
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/mainboard/lenovo/w541/romstage.c
+++ src/mainboard/lenovo/w541/romstage.c
@@ -1,11 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <stdint.h>
-#include <cpu/intel/haswell/haswell.h>
 #include <northbridge/intel/haswell/haswell.h>
-#include <northbridge/intel/haswell/pei_data.h>
 #include <northbridge/intel/haswell/raminit.h>
-#include <southbridge/intel/common/gpio.h>
 #include <southbridge/intel/lynxpoint/pch.h>
 
 void mainboard_config_rcba(void)
@@ -21,7 +18,6 @@ void mainboard_config_rcba(void)
 }
 void mainboard_fill_pei_data(struct pei_data *pei_data)
 {
-	pei_data->system_type = 0; /* Mobile */
 	pei_data->spd_addresses[0] = 0xa0;
 	pei_data->spd_addresses[1] = 0xa2;
 	pei_data->spd_addresses[2] = 0xa4;
